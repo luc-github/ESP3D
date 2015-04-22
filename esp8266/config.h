6321175d069d295a1430514149f3dad85b4c45cc
luc lebosse <luc.lebosse@tech-hunters.com>
2015-04-22 16:59:15 +0800

    Fix IP String Size in EEPROM
    Fix IP String Size in EEPROM

--- esp8266/config.h
+++ esp8266/config.h
@@ -43,9 +43,9 @@
 #define EP_SSID            1    //33 bytes 32+1 = string  ; warning does not support multibyte char like chinese
 #define EP_PASSWORD        34   //65 bytes 64 +1 = string ;warning does not support multibyte char like chinese
 #define EP_IP_MODE         99   //1 byte = flag
-#define EP_IP_VALUE        100  //17 bytes xxx.xxx.xxx\0 = string
-#define EP_MASK_VALUE      117  //17 bytes xxx.xxx.xxx\0 = string
-#define EP_GATEWAY_VALUE   134  //17 bytes xxx.xxx.xxx\0 = string
+#define EP_IP_VALUE        100  //16 bytes xxx.xxx.xxx\0 = string
+#define EP_MASK_VALUE      116  //16 bytes xxx.xxx.xxx\0 = string
+#define EP_GATEWAY_VALUE   132  //16 bytes xxx.xxx.xxx\0 = string
 #define EP_BAUD_RATE       151  //7 bytes = string (if integer value => save 4 bytes but need to create new integer function for eeprom that will take more than 4 bytes)
 
 //default values
