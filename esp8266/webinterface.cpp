8dd8ec207d602a81f378cc9272083ad0861f68e4
Luc <luc.lebosse@gmail.com>
2015-08-03 22:10:42 +0800

    Fix Case sensitive issue for linux/mac thanks @c128
    follow case of file name

--- esp8266/webinterface.cpp
+++ esp8266/webinterface.cpp
@@ -18,7 +18,7 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include <PgmSpace.h>
+#include <pgmspace.h>
 #include "config.h"
 #include "webinterface.h"
 #include "wifi.h"
