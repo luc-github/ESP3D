10e3cf16740f8e9123f72568bf8a2175a1484245
luc <luc.lebosse@tech-hunters.com>
2015-10-09 22:28:46 +0800

    Fix typo issue - bad copy and past SIG

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -12,7 +12,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Repetier-Firmware.  If not, see <http://www.gnu.org/licenses/>.
+    along with  this Firmware.  If not, see <http://www.gnu.org/licenses/>.
 
     This firmware is using the standard arduino IDE with module to support ESP8266:
     https://github.com/esp8266/Arduino from Bootmanager
