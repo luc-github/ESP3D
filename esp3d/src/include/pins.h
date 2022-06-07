1b722775695b36564f134e498903383fef00ad54
Luc <8822552+luc-github@users.noreply.github.com>
2022-06-07 14:05:14 +0800

    Add latest Camera pins
    Note: There is no platformIO env defined for Camera on S2/S3 MCU as I cannot test them

--- esp3d/src/include/pins.h
+++ esp3d/src/include/pins.h
@@ -46,6 +46,10 @@
 #endif //~ESP_SCL_PIN
 
 //Pins for the support of connected camera
+#ifndef USE_BOARD_HEADER
+#define USE_BOARD_HEADER 0
+#endif //USE_BOARD_HEADER
+
 #if CAMERA_DEVICE == CAMERA_MODEL_CUSTOM
 #define CAM_LED_PIN       4
 #define CAM_PULLUP1       -1
@@ -138,6 +142,29 @@
 #define PCLK_GPIO_NUM     21
 #endif //CAMERA_MODEL_M5STACK_PSRAM
 
+#if CAMERA_DEVICE == CAMERA_MODEL_M5STACK_PSRAM_2
+#define CAM_LED_PIN       -1
+#define CAM_PULLUP1       -1
+#define CAM_PULLUP2       -1
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    15
+#define XCLK_GPIO_NUM     27
+#define SIOD_GPIO_NUM     22
+#define SIOC_GPIO_NUM     23
+
+#define Y9_GPIO_NUM       19
+#define Y8_GPIO_NUM       36
+#define Y7_GPIO_NUM       18
+#define Y6_GPIO_NUM       39
+#define Y5_GPIO_NUM        5
+#define Y4_GPIO_NUM       34
+#define Y3_GPIO_NUM       35
+#define Y2_GPIO_NUM       32
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     26
+#define PCLK_GPIO_NUM     21
+#endif //CAMERA_MODEL_M5STACK_PSRAM_2
+
 #if CAMERA_DEVICE == CAMERA_MODEL_M5STACK_WIDE
 #define CAM_LED_PIN       -1
 #define CAM_PULLUP1       -1
@@ -161,6 +188,53 @@
 #define PCLK_GPIO_NUM     21
 #endif //CAMERA_MODEL_M5STACK_WIDE
 
+#if CAMERA_DEVICE == CAMERA_MODEL_M5STACK_ESP32CAM
+#define CAM_LED_PIN       -1
+#define CAM_PULLUP1       -1
+#define CAM_PULLUP2       -1
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    15
+#define XCLK_GPIO_NUM     27
+#define SIOD_GPIO_NUM     25
+#define SIOC_GPIO_NUM     23
+
+#define Y9_GPIO_NUM       19
+#define Y8_GPIO_NUM       36
+#define Y7_GPIO_NUM       18
+#define Y6_GPIO_NUM       39
+#define Y5_GPIO_NUM        5
+#define Y4_GPIO_NUM       34
+#define Y3_GPIO_NUM       35
+#define Y2_GPIO_NUM       17
+#define VSYNC_GPIO_NUM    22
+#define HREF_GPIO_NUM     26
+#define PCLK_GPIO_NUM     21
+
+#endif //CAMERA_MODEL_M5STACK_ESP32CAM
+
+#if CAMERA_DEVICE == CAMERA_MODEL_M5STACK_UNITCAM
+#define CAM_LED_PIN       -1
+#define CAM_PULLUP1       -1
+#define CAM_PULLUP2       -1
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    15
+#define XCLK_GPIO_NUM     27
+#define SIOD_GPIO_NUM     25
+#define SIOC_GPIO_NUM     23
+
+#define Y9_GPIO_NUM       19
+#define Y8_GPIO_NUM       36
+#define Y7_GPIO_NUM       18
+#define Y6_GPIO_NUM       39
+#define Y5_GPIO_NUM        5
+#define Y4_GPIO_NUM       34
+#define Y3_GPIO_NUM       35
+#define Y2_GPIO_NUM       32
+#define VSYNC_GPIO_NUM    22
+#define HREF_GPIO_NUM     26
+#define PCLK_GPIO_NUM     21
+#endif //CAMERA_MODEL_M5STACK_UNITCAM
+
 #if CAMERA_DEVICE == CAMERA_MODEL_AI_THINKER
 #define CAM_LED_PIN       -1 //used by SD so must left unset
 #define CAM_PULLUP1       -1
@@ -183,6 +257,142 @@
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 #endif //CAMERA_MODEL_AI_THINKER
+
+#if CAMERA_DEVICE == CAMERA_MODEL_TTGO_T_JOURNAL
+#define CAM_LED_PIN       -1
+#define CAM_PULLUP1       -1
+#define CAM_PULLUP2       -1
+#define PWDN_GPIO_NUM      0
+#define RESET_GPIO_NUM    15
+#define XCLK_GPIO_NUM     27
+#define SIOD_GPIO_NUM     25
+#define SIOC_GPIO_NUM     23
+
+#define Y9_GPIO_NUM       19
+#define Y8_GPIO_NUM       36
+#define Y7_GPIO_NUM       18
+#define Y6_GPIO_NUM       39
+#define Y5_GPIO_NUM        5
+#define Y4_GPIO_NUM       34
+#define Y3_GPIO_NUM       35
+#define Y2_GPIO_NUM       17
+#define VSYNC_GPIO_NUM    22
+#define HREF_GPIO_NUM     26
+#define PCLK_GPIO_NUM     21
+#endif //CAMERA_MODEL_TTGO_T_JOURNAL
+
+#if CAMERA_DEVICE == CAMERA_MODEL_ESP32_CAM_BOARD
+#define CAM_LED_PIN       -1
+#define CAM_PULLUP1       -1
+#define CAM_PULLUP2       -1
+// The 18 pin header on the board has Y5 and Y3 swapped
+#define USE_BOARD_HEADER 0
+#define PWDN_GPIO_NUM    32
+#define RESET_GPIO_NUM   33
+#define XCLK_GPIO_NUM     4
+#define SIOD_GPIO_NUM    18
+#define SIOC_GPIO_NUM    23
+
+#define Y9_GPIO_NUM      36
+#define Y8_GPIO_NUM      19
+#define Y7_GPIO_NUM      21
+#define Y6_GPIO_NUM      39
+#if USE_BOARD_HEADER
+#define Y5_GPIO_NUM      13
+#else
+#define Y5_GPIO_NUM      35
+#endif
+#define Y4_GPIO_NUM      14
+#if USE_BOARD_HEADER
+#define Y3_GPIO_NUM      35
+#else
+#define Y3_GPIO_NUM      13
+#endif
+#define Y2_GPIO_NUM      34
+#define VSYNC_GPIO_NUM    5
+#define HREF_GPIO_NUM    27
+#define PCLK_GPIO_NUM    25
+#endif //CAMERA_MODEL_ESP32_CAM_BOARD
+
+#if CAMERA_DEVICE == CAMERA_MODEL_ESP32S3_CAM_LCD
+#define CAM_LED_PIN       -1
+#define CAM_PULLUP1       -1
+#define CAM_PULLUP2       -1
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM     40
+#define SIOD_GPIO_NUM     17
+#define SIOC_GPIO_NUM     18
+
+#define Y9_GPIO_NUM       39
+#define Y8_GPIO_NUM       41
+#define Y7_GPIO_NUM       42
+#define Y6_GPIO_NUM       12
+#define Y5_GPIO_NUM       3
+#define Y4_GPIO_NUM       14
+#define Y3_GPIO_NUM       47
+#define Y2_GPIO_NUM       13
+#define VSYNC_GPIO_NUM    21
+#define HREF_GPIO_NUM     38
+#define PCLK_GPIO_NUM     11
+#endif //CAMERA_MODEL_ESP32S3_CAM_LCD
+
+#if CAMERA_DEVICE == CAMERA_MODEL_ESP32S2_CAM_BOARD
+#define CAM_LED_PIN       -1
+#define CAM_PULLUP1       -1
+#define CAM_PULLUP2       -1
+// The 18 pin header on the board has Y5 and Y3 swapped
+#define PWDN_GPIO_NUM     1
+#define RESET_GPIO_NUM    2
+#define XCLK_GPIO_NUM     42
+#define SIOD_GPIO_NUM     41
+#define SIOC_GPIO_NUM     18
+
+#define Y9_GPIO_NUM       16
+#define Y8_GPIO_NUM       39
+#define Y7_GPIO_NUM       40
+#define Y6_GPIO_NUM       15
+#if USE_BOARD_HEADER
+#define Y5_GPIO_NUM       12
+#else
+#define Y5_GPIO_NUM       13
+#endif
+#define Y4_GPIO_NUM       5
+#if USE_BOARD_HEADER
+#define Y3_GPIO_NUM       13
+#else
+#define Y3_GPIO_NUM       12
+#endif
+#define Y2_GPIO_NUM       14
+#define VSYNC_GPIO_NUM    38
+#define HREF_GPIO_NUM     4
+#define PCLK_GPIO_NUM     3
+#endif //CAMERA_MODEL_ESP32S2_CAM_BOARD
+
+#if CAMERA_DEVICE == CAMERA_MODEL_ESP32S3_EYE
+#define CAM_LED_PIN       -1
+#define CAM_PULLUP1       -1
+#define CAM_PULLUP2       -1
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 15
+#define SIOD_GPIO_NUM 4
+#define SIOC_GPIO_NUM 5
+
+#define Y2_GPIO_NUM 11
+#define Y3_GPIO_NUM 9
+#define Y4_GPIO_NUM 8
+#define Y5_GPIO_NUM 10
+#define Y6_GPIO_NUM 12
+#define Y7_GPIO_NUM 18
+#define Y8_GPIO_NUM 17
+#define Y9_GPIO_NUM 16
+
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM 7
+#define PCLK_GPIO_NUM 13
+#endif //CAMERA_MODEL_ESP32S3_EYE
+
 
 //Pins for the support of SD Card Reader
 //-1 means use default pins of your board defined core
