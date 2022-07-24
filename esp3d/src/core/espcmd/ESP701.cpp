5eb93f6aee99f4bdf79b2e64bcc2eaf28b133e09
James Pearson <j.c.pearson@swansea.ac.uk>
2022-07-24 23:45:34 +0100

    -Added HOST_WAIT4_HEATING to switch. -Fixed broken (else) if statement resulting in incorrect response message.

--- esp3d/src/core/espcmd/ESP701.cpp
+++ esp3d/src/core/espcmd/ESP701.cpp
@@ -46,7 +46,7 @@ bool Commands::ESP701(const char* cmd_params, level_authenticate_type auth_type,
     (void)auth_type;
 #endif //AUTHENTICATION_FEATURE
     if (noError) {
-        parameter = (get_param (cmd_params, "action="));
+        parameter = (get_param(cmd_params, "action="));
         if (parameter.length() != 0) {
             if (parameter.equalsIgnoreCase("PAUSE")) {
                 if (esp3d_gcode_host.pause()) {
@@ -69,8 +69,7 @@ bool Commands::ESP701(const char* cmd_params, level_authenticate_type auth_type,
                     response = format_response(COMMANDID, json, false, "No stream to abort");
                     noError = false;
                 }
-            }
-            if (parameter.equalsIgnoreCase("CLEAR_ERROR")) {
+            } else if (parameter.equalsIgnoreCase("CLEAR_ERROR")) {
                 esp3d_gcode_host.setErrorNum(ERROR_NO_ERROR);
                 response = format_response(COMMANDID, json, true, "Error cleared");
             } else {
@@ -86,6 +85,7 @@ bool Commands::ESP701(const char* cmd_params, level_authenticate_type auth_type,
             case HOST_READ_LINE:
             case HOST_PROCESS_LINE:
             case HOST_WAIT4_ACK:
+            case HOST_WAIT4_HEATING:
                 //TODO add % of progress and filename if any
                 //totalSize / processedSize / fileName
                 if (json) {
