f442871b0874bb5193dc3d71558c90923eeb5c9f
Luc <8822552+luc-github@users.noreply.github.com>
2023-11-05 16:07:14 +0800

    Add PUT method

--- esp3d/src/modules/webdav/handlers/handler_put.cpp
+++ esp3d/src/modules/webdav/handlers/handler_put.cpp
@@ -21,13 +21,133 @@
 #include "../../../include/esp3d_config.h"
 
 #if defined(WEBDAV_FEATURE)
+#include "../../../core/esp3d_string.h"
 #include "../webdav_server.h"
+#if defined(HTTP_FEATURE)
+#include "../../websocket/websocket_server.h"
+#endif  // HTTP_FEATURE
 
 void WebdavServer::handler_put(const char* url) {
-  log_esp3d("Processing PUT");
-  clearPayload();
-  send_response_code(200);
-  send_webdav_headers();
+  log_esp3d_d("Processing PUT");
+  int code = 201;
+  size_t content_length = 0;
+  uint8_t fsType = WebDavFS::getFSType(url);
+  log_esp3d("FS type of %s : %d", url, fsType);
+  if (hasHeader("Content-Length")) {
+    content_length = atoi(getHeader("Content-Length"));
+    log_esp3d("Content-Length: %d", content_length);
+  } else {
+    log_esp3d("Depth not set");
+  }
+
+  // url cannot be root
+  if (!isRoot(url)) {
+    if (WebDavFS::accessFS(fsType)) {
+      bool hasError = false;
+      // check if file exists
+      if (WebDavFS::exists(url)) {
+        log_esp3d("File %s already exists", url);
+        code = 204;
+        WebDavFile file = WebDavFS::open(url);
+        if (file) {
+          if (file.isDirectory()) {
+            code = 412;
+            log_esp3d_e("File %s is a directory", url);
+            hasError = true;
+          }
+          file.close();
+          if (!hasError) {
+            // check size available
+            if (WebDavFS::freeBytes(fsType) + file.size() < content_length) {
+              code = 507;
+              log_esp3d_e("Not enough space");
+              hasError = true;
+            } else {  // there is enough space
+              if (!WebDavFS::remove(
+                      url)) {  // so delete existing file to replace it
+                code = 500;
+                log_esp3d_e("Failed to remove %s", url);
+                hasError = true;
+              }
+            }
+          }
+        } else {
+          hasError = true;
+          code = 500;
+          log_esp3d_e("Failed to open %s", url);
+        }
+      } else {
+        // check size available
+        if (WebDavFS::freeBytes(fsType) < content_length) {
+          code = 507;
+          log_esp3d_e("Not enough space");
+          hasError = true;
+        }
+      }
+      if (!hasError) {  // read  payload and write it to file
+        WebDavFile file = WebDavFS::open(url, ESP_FILE_WRITE);
+        if (file) {
+          size_t received = 0;
+#if defined(ARDUINO_ARCH_ESP32)
+          uint8_t chunk[2048];
+#endif  // ARDUINO_ARCH_ESP32
+#if defined(ARDUINO_ARCH_ESP8266)
+          uint8_t chunk[512];
+#endif  // ARDUINO_ARCH_ESP8266
+#if defined(HTTP_FEATURE)
+          Esp3dTimout updateWS(2000);
+#endif  // HTTP_FEATURE
+          while (_client.available() && received < content_length) {
+            Hal::wait(0);
+            size_t received_bytes = _client.read(chunk, sizeof(chunk));
+            if (received_bytes != file.write(chunk, received_bytes)) {
+              code = 500;
+              log_esp3d_e("Failed to write %s", url);
+              break;
+            } else {
+              received += received_bytes;
+            }
+
+#if defined(HTTP_FEATURE)
+            // update websocket every 2000 ms
+            if (updateWS.isTimeout()) {
+              websocket_terminal_server.handle();
+              updateWS.reset();
+            }
+#endif  // HTTP_FEATURE
+          }
+          if (received != content_length) {
+            code = 500;
+            log_esp3d_e("Failed to write %s", url);
+          } else {
+            log_esp3d("File %s written", url);
+            send_response_code(code);//201 or 204
+            send_webdav_headers();
+            time_t now = time(nullptr);
+            send_header("Last-Modified",
+                        esp3d_string::getTimeString(now, true));
+          }
+
+          file.close();
+        } else {
+          code = 500;
+          log_esp3d_e("Failed to open %s for writing", url);
+        }
+      }
+      WebDavFS::releaseFS(fsType);
+    } else {
+      code = 503;
+      log_esp3d_e("FS not available");
+    }
+  } else {
+    code = 400;
+    log_esp3d_e("Root cannot be deleted");
+  }
+  if (code != 201 && code != 204) {
+    log_esp3d_e("Sending response code %d", code);
+    send_response_code(code);
+    send_webdav_headers();
+  }
 }
 
 #endif  // WEBDAV_FEATURE
