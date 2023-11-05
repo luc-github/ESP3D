625cf9ccd33e98f18e036175d5f8d804f83fd575
Luc <8822552+luc-github@users.noreply.github.com>
2023-11-05 16:42:02 +0800

    Add MOVE method

--- esp3d/src/modules/webdav/handlers/handler_move.cpp
+++ esp3d/src/modules/webdav/handlers/handler_move.cpp
@@ -21,13 +21,84 @@
 #include "../../../include/esp3d_config.h"
 
 #if defined(WEBDAV_FEATURE)
+#include "../../../core/esp3d_string.h"
 #include "../webdav_server.h"
 
 void WebdavServer::handler_move(const char* url) {
-  log_esp3d("Processing MOVE");
-  clearPayload();
-  send_response_code(200);
-  send_webdav_headers();
+  log_esp3d_d("Processing MOVE");
+  int code = 201;
+  size_t sp = clearPayload();
+  log_esp3d("Payload size: %d", sp);
+  uint8_t fsTypeOrigin = WebDavFS::getFSType(url);
+  log_esp3d("FS type of %s : %d", url, fsTypeOrigin);
+  String destination = "";
+  if (hasHeader("Destination")) {
+    destination = getHeader("Destination");
+    log_esp3d("Destination: %s", destination.c_str());
+    uint8_t fsTypeDestination = WebDavFS::getFSType(destination.c_str());
+    log_esp3d("FS type of %s : %d", destination.c_str(), fsTypeDestination);
+    if (fsTypeDestination != fsTypeOrigin) {
+      code = 409;
+      log_esp3d_e("Destination and origin must be on same FS");
+    } else {
+      bool overwrite = false;
+      if (hasHeader("Overwrite")) {
+        overwrite = (strcmp(getHeader("Overwrite"), "T") == 0);
+        log_esp3d("Overwrite: %s", overwrite ? "true" : "false");
+      }
+      // url cannot be root
+      if (!isRoot(url) && !isRoot(destination.c_str())) {
+        if (WebDavFS::accessFS(fsTypeOrigin)) {
+          // check if file exists
+          if (WebDavFS::exists(url)) {
+            if (WebDavFS::exists(destination.c_str())) {
+              log_esp3d("Destination already exists");
+              if (!overwrite) {
+                code = 412;
+                log_esp3d_e("Overwrite not allowed");
+              } else {
+                log_esp3d("Overwrite allowed");
+                code = 204;
+                if (!WebDavFS::remove(destination.c_str())) {
+                  code = 500;
+                  log_esp3d_e("Failed to remove %s", destination.c_str());
+                  overwrite = false;
+                }
+              }
+            } else {
+              log_esp3d("Destination does not exist");
+              overwrite = true;
+            }
+            if (overwrite) {
+              if (!WebDavFS::rename(url, destination.c_str())) {
+                code = 500;
+                log_esp3d_e("Failed to move %s to %s", url,
+                            destination.c_str());
+              }
+            }
+          } else {
+            code = 404;
+            log_esp3d_e("File not found");
+          }
+          WebDavFS::releaseFS(fsTypeOrigin);
+        } else {
+          code = 503;
+          log_esp3d_e("FS not available");
+        }
+      } else {
+        code = 400;
+        log_esp3d_e("Root cannot be deleted");
+      }
+    }
+  } else {
+    code = 400;
+    log_esp3d_e("Destination not set");
+  }
+  if (code != 201 && code != 204) {
+    log_esp3d_e("Sending response code %d", code);
+    send_response_code(code);
+    send_webdav_headers();
+  }
 }
 
 #endif  // WEBDAV_FEATURE
