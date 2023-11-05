0f76bc9a51c8acbe49ac4473d2f264c34203b0f2
Luc <8822552+luc-github@users.noreply.github.com>
2023-11-05 13:41:35 +0800

    Add HEAD method

--- esp3d/src/modules/webdav/handlers/handler_head.cpp
+++ esp3d/src/modules/webdav/handlers/handler_head.cpp
@@ -21,13 +21,53 @@
 #include "../../../include/esp3d_config.h"
 
 #if defined(WEBDAV_FEATURE)
+#include "../../../core/esp3d_string.h"
 #include "../webdav_server.h"
 
 void WebdavServer::handler_head(const char* url) {
-  log_esp3d("Processing HEAD");
-  clearPayload();
-  send_response_code(200);
-  send_webdav_headers();
+  log_esp3d_d("Processing HEAD");
+  int code = 200;
+  size_t sp = clearPayload();
+  log_esp3d("Payload size: %d", sp);
+  uint8_t fsType = WebDavFS::getFSType(url);
+  log_esp3d("FS type of %s : %d", url, fsType);
+  // url cannot be root
+  if (!isRoot(url)) {
+    if (WebDavFS::accessFS(fsType)) {
+      // check if file exists
+      if (WebDavFS::exists(url)) {
+        WebDavFile file = WebDavFS::open(url);
+        if (file) {
+          // send response
+          send_response_code(code);
+          send_webdav_headers();
+          send_header("Last-Modified", esp3d_string::getTimeString(
+                                           (time_t)file.getLastWrite(), true));
+          send_header("Content-Length", String(file.size()).c_str());
+          send_header("Content-Type", esp3d_string::getContentType(url));
+          file.close();
+        } else {
+          code = 500;
+          log_esp3d_e("Failed to open %s", url);
+        }
+      } else {
+        code = 404;
+        log_esp3d_e("File not found");
+      }
+      WebDavFS::releaseFS(fsType);
+    } else {
+      code = 503;
+      log_esp3d_e("FS not available");
+    }
+  } else {
+    code = 400;
+    log_esp3d_e("Root cannot be deleted");
+  }
+  if (code != 200) {
+    log_esp3d_e("Sending response code %d", code);
+    send_response_code(code);
+    send_webdav_headers();
+  }
 }
 
 #endif  // WEBDAV_FEATURE
