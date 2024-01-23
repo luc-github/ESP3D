3787501a4b9195e75a5d481e6b6895d3c8e6a808
E2D <engineer2designer@gmail.com>
2024-01-23 12:20:18 +0800

    Update esp3d_commands.cpp
    fix [ESP250] command,if no DISPLAY_DEVICE.
    
    Signed-off-by: E2D <engineer2designer@gmail.com>

--- esp3d/src/core/esp3d_commands.cpp
+++ esp3d/src/core/esp3d_commands.cpp
@@ -609,6 +609,7 @@ void ESP3DCommands::execute_internal_command(int cmd, int cmd_params_pos,
       ESP215(cmd_params_pos, msg);
       break;
 #endif  // DISPLAY_TOUCH_DRIVER
+#endif  // DISPLAY_DEVICE    
 #ifdef BUZZER_DEVICE
     // Play sound
     //[ESP250]F=<frequency> D=<duration> [pwd=<user password>]
@@ -616,7 +617,6 @@ void ESP3DCommands::execute_internal_command(int cmd, int cmd_params_pos,
       ESP250(cmd_params_pos, msg);
       break;
 #endif  // BUZZER_DEVICE
-#endif  // DISPLAY_DEVICE
     // Show pins
     //[ESP220][pwd=<user password>]
     case 220:
