d8f84b94d95d4f48d5dd75be39785bd0153ebe4b
Luc <8822552+luc-github@users.noreply.github.com>
2024-02-05 14:09:13 +0800

    Fix issue raised by @rondlh

--- esp3d/src/modules/websocket/websocket_server.cpp
+++ esp3d/src/modules/websocket/websocket_server.cpp
@@ -80,7 +80,7 @@ bool WebSocket_Server::pushMSG(uint num, const char *data) {
 
 bool WebSocket_Server::isConnected(){
   if (_websocket_server) {
-    return _websocket_server->connectedClients(true) > 0;
+    return _websocket_server->connectedClients() > 0;
   }
   return false;
 }
