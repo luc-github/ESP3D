6a9798729558f04379f08df3b5a5a51f34aed526
E2D <engineer2designer@gmail.com>
2025-03-14 23:11:18 +0800

    Update buzzer.cpp

--- esp3d/src/modules/buzzer/buzzer.cpp
+++ esp3d/src/modules/buzzer/buzzer.cpp
@@ -54,15 +54,8 @@ bool BuzzerDevice::begin() {
     end();
   }
 #if defined(ARDUINO_ARCH_ESP32)
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 1, 1) {
   ledcAttachChannel(0, 12000, 8, 0);
-#else
-  setToneChannel(0);
-  ledcSetup(0, 12000, 8);
-}
-#endif // ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 1, 1)
 #endif // defined(ARDUINO_ARCH_ESP32)
-
   if (ESP3DSettings::readByte(ESP_BUZZER) == 1) {
     _started = true;
     playsound(5000, 240);
